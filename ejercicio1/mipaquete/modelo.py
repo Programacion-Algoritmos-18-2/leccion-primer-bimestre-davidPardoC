bf796b3089844890d14dfad6d0191f5a0c5bc300
David Pardo <pardodavid10@gmail.com>
2018-11-09 11:14:53 -0500

    Empleado por horas

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -3,7 +3,7 @@ class Empleado(object):
         self.nombre=" "
         self.apellido=" "
         self.cedula=" "
-        self.comision_fija=0
+        self.comision_fija=300
 
     def agregar_comision(self, comision):
         self.comision_fija=comision
@@ -31,4 +31,30 @@ class Empleado(object):
 
     def presentar_datos(self):
         cadena="Informacion de %s %s \n\t cedula %s"%(self.obtener_nombre(), self.obtener_apellido(), self.cedula)
-        return cadena;
+        return cadena
+
+class EmpleadoPorHoras(Empleado):
+    def __init__(self):
+        super(EmpleadoPorHoras, self).__init__()
+        self.num_Horas = 0
+        self.valorHoras = 0
+
+    def agregar_numero_horas(self, h):
+        self.num_Horas = h
+
+    def obtener_num_horas(self):
+        return self.num_Horas
+
+    def agregar_valor_hora(self, v):
+        self.valorHoras=v
+
+    def obtener_valor_horas(self):
+        return self.valorHoras
+
+    def calcular_sueldo(self):
+        sueldo = (self.num_Horas*self.valorHoras)+self.comision_fija
+        return sueldo
+
+    def presentar_datos(self):
+        cadena="%s \n Numero horas: %s \n Valor Hora: %s \n Sueldo Final: %s"%(super(EmpleadoPorHoras, self).presentar_datos(), self.obtener_num_horas(), self.obtener_valor_horas(), self.calcular_sueldo())
+        return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -4,3 +4,16 @@ e.agregar_nombre("luis")
 e.agregar_apellido("Benitez")
 e.agregar_cedula("1110001")
 print(e.presentar_datos())
+
+print("\nEmpleado Por Horas:\n")
+
+e1=EmpleadoPorHoras()
+nombre=input("Ingrese el nomnre: ")
+e1.agregar_nombre(nombre)
+e1.agregar_apellido("Andrade")
+e1.agregar_cedula("112233")
+e1.agregar_numero_horas(20.2)
+e1.agregar_valor_hora(15)
+print(e1.presentar_datos())
+
+
