59650e2a44443d0bcb8a2e4d54ceeadd6403864b
David Pardo <pardodavid10@gmail.com>
2018-11-09 11:56:20 -0500

    Todo

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -1,3 +1,4 @@
+#Calse Empleado(Padre)
 class Empleado(object):
     def __init__(self):
         self.nombre=" "
@@ -32,13 +33,14 @@ class Empleado(object):
     def presentar_datos(self):
         cadena="Informacion de %s %s \n\t cedula %s"%(self.obtener_nombre(), self.obtener_apellido(), self.cedula)
         return cadena
-
+#Calse EmpleadoPorHoras(Hija)
 class EmpleadoPorHoras(Empleado):
     def __init__(self):
         super(EmpleadoPorHoras, self).__init__()
         self.num_Horas = 0
         self.valorHoras = 0
 
+#getters y setters
     def agregar_numero_horas(self, h):
         self.num_Horas = h
 
@@ -50,11 +52,72 @@ class EmpleadoPorHoras(Empleado):
 
     def obtener_valor_horas(self):
         return self.valorHoras
-
+#calcula Sueldo
     def calcular_sueldo(self):
         sueldo = (self.num_Horas*self.valorHoras)+self.comision_fija
         return sueldo
 
     def presentar_datos(self):
         cadena="%s \n Numero horas: %s \n Valor Hora: %s \n Sueldo Final: %s"%(super(EmpleadoPorHoras, self).presentar_datos(), self.obtener_num_horas(), self.obtener_valor_horas(), self.calcular_sueldo())
+        return cadena
+#Clase EpleadoFijo(Hija)
+class EmpleadoFijo(Empleado):
+    def __init__(self):
+        super(EmpleadoFijo, self). __init__()
+        self.sueldofijo=0
+        self.descuento_seguro=0
+
+#getters y setters
+
+    def agregar_sueldo_fijo(self, s):
+        self.sueldofijo=s
+
+    def obtener_sueldo_fijo(self):
+        return self.sueldofijo
+
+    def agregar_descuento(self, d):
+        self.descuento_seguro=d
+
+    def obtener_descuento(self):
+        return self.descuento_seguro
+
+#Clacula el sueldo
+
+    def calcular_sueldo(self):
+        sueldo = (self.comision_fija+self.sueldofijo)-((self.comision_fija+self.sueldofijo)*(self.descuento_seguro/100))+self.comision_fija
+        return sueldo
+
+    def presentar_datos(self):
+        cadena="%s \n Sueldo Final: %s"%(super(EmpleadoFijo, self).presentar_datos(), self.calcular_sueldo())
+        return cadena
+
+#Clase EmpleadoSemnal(Hija)
+class EmpleadoSemanal(Empleado):
+    def __init__(self):
+        super(EmpleadoSemanal, self).__init__()
+        self.numero_semanas=0
+        self.valor_semana=0
+
+#Setters y Getters
+
+    def agregar_numero_semanas(self, n):
+        self.numero_semanas=n
+
+    def obtener_numero_semanas(self):
+        return self.numero_semanas
+
+    def agregar_valor_semana(self, v):
+        self.valor_semana=v
+
+    def obtener_valor_semana(self):
+        return self.valor_semana
+
+#Calcula Sueldo Final
+
+    def sueldo_final(self):
+        sueldo=(self.valor_semana*self.numero_semanas)+self.comision_fija
+        return sueldo
+
+    def presentar_datos(self):
+        cadena="%s \n Sueldo Final: %s"%(super(EmpleadoSemanal, self).presentar_datos(), self.sueldo_final())
         return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -1,4 +1,6 @@
+#Importar Modulo
 from mipaquete.modelo import *
+#Crea Empleado
 e=Empleado()
 e.agregar_nombre("luis")
 e.agregar_apellido("Benitez")
@@ -7,6 +9,7 @@ print(e.presentar_datos())
 
 print("\nEmpleado Por Horas:\n")
 
+#Crea Empleado por horas
 e1=EmpleadoPorHoras()
 nombre=input("Ingrese el nomnre: ")
 e1.agregar_nombre(nombre)
@@ -16,4 +19,26 @@ e1.agregar_numero_horas(20.2)
 e1.agregar_valor_hora(15)
 print(e1.presentar_datos())
 
+print("\nEmpleado Fijo\n")
+#Crea Empleado Fijo
+e2=EmpleadoFijo()
+e2.agregar_sueldo_fijo(3002)
+e2.agregar_descuento(10)
+e2.agregar_cedula("112233")
+comision=input("Comision: ")
+comision=int(comision)
+e2.agregar_comision(comision)
+e2.agregar_nombre("Ana")
+e2.agregar_apellido("Diaz")
+print(e2.presentar_datos())
 
+print("\n EMpleado SEmanal\n")
+#Crea Empleado Semnanal
+e3=EmpleadoSemanal()
+e3.agregar_nombre("David")
+e3.agregar_apellido("Pardo")
+e3.agregar_cedula("1111")
+e3.agregar_numero_semanas(4)
+e3.agregar_valor_semana(100)
+e3.agregar_comision(300)
+print(e3.presentar_datos())
